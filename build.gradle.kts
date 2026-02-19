1b161ec3428cec8c1db2bc6d3178b5d4053f3ee8
Kimdonghwan <kimdonghwan913@gmail.com>
2026-02-20 01:07:09 +0900

    docs: 도메인 모델 설계

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.10"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "tobyspring"
+version = "0.0.1-SNAPSHOT"
+description = "클린 스프링"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
