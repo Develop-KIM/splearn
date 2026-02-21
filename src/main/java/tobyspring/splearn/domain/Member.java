a1281f1ff83961bd9ac4164b20577145f594f14d
Kimdonghwan <kimdonghwan913@gmail.com>
2026-02-21 18:04:21 +0900

    feat: 회원 애플리케이션 서비스 구현

--- src/main/java/tobyspring/splearn/domain/Member.java
+++ src/main/java/tobyspring/splearn/domain/Member.java
@@ -10,10 +10,7 @@ import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.hibernate.annotations.NaturalId;
-import org.hibernate.annotations.NaturalIdCache;
 
 import static java.util.Objects.requireNonNull;
 import static org.springframework.util.Assert.state;
--- src/main/java/tobyspring/splearn/domain/PasswordEncoder.java
+++ src/main/java/tobyspring/splearn/domain/PasswordEncoder.java
@@ -1,5 +1,8 @@
 package tobyspring.splearn.domain;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface PasswordEncoder {
     String encode(String password);
     boolean matches(String password, String passwordHash);
